d8abdf90038bf3eac25b3195332f15ad0c9029e4
ericgaspar <junk.eg@free.fr>
2020-03-24 16:12:04 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3-pip"
+pkg_dependencies="python3-pip libffi-dev"
 
 #=================================================
 # PERSONAL HELPERS
