62f5619ad14ff1d04019c55e81f1e73a41ee7176
Kay0u <pierre@kayou.io>
2020-06-22 14:31:44 +0200

    add python3-dev

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3-pip libffi-dev libzmq3-dev"
+pkg_dependencies="python3-dev python3-pip libffi-dev libzmq3-dev"
 
 nodejs_version="10"
 
