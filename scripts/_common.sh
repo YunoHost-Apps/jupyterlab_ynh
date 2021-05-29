23f8695b950d1a432d05529d60492f6f0cedf01d
Jules-Bertholet <jules.bertholet@gmail.com>
2021-05-29 15:07:22 +0000

    Fix permissions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,6 +15,20 @@ jupyterlab_version="3.0.14"
 # PERSONAL HELPERS
 #=================================================
 
+function python_setup {
+	pushd "$final_path"
+		sudo -u $app PIPENV_VENV_IN_PROJECT="enabled" PIPENV_SKIP_LOCK=true  python3 -m pipenv install jupyterlab==$jupyterlab_version jupyterhub notebook jupyterhub-ldapauthenticator pyzmq --three 2>&1
+		sudo -u $app python3 -m pipenv run jupyterhub upgrade-db 2>&1
+	popd
+}
+
+function set_permissions {
+	# Set permissions to app files
+	chown -R root:$app "$final_path"
+	chown -R $app:$app "$final_path/.venv"
+	chmod -R g=u,g-w,o-rwx "$final_path"
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
