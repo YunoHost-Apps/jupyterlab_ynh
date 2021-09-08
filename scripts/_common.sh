3e5cba92ab381a6b89e88d4c761a1a2fd423694b
ericgaspar <junk.eg@free.fr>
2021-09-08 17:50:10 +0200

    3.1.11

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ pkg_dependencies="python3-dev python3-pip libffi-dev libzmq3-dev"
 
 nodejs_version="15"
 
-jupyterlab_version="3.1.10"
+jupyterlab_version="3.1.11"
 
 #=================================================
 # PERSONAL HELPERS
