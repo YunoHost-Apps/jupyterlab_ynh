3dce5acdf2c2370bdfd25bc769a1a3a8f6dba5bc
Kay0u <pierre@kayou.io>
2023-08-16 23:55:40 +0200

    Upgrade to 4.0.5

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ pkg_dependencies="python3-dev python3-pip python3-venv"
 
 nodejs_version="18"
 
-jupyterlab_version="4.0.2"
+jupyterlab_version="4.0.5"
 
 #=================================================
 # PERSONAL HELPERS
