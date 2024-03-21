a7c9218e639bf2b3d4e8c791f35cc366a90edcf8
Félix Piédallu <felix@piedallu.me>
2024-03-21 23:29:19 +0100

    Cleanup, apply template

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,11 +6,17 @@
 
 nodejs_version="18"
 
-jupyterlab_version="4.1.0"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+upstream_version() {
+    upstream_repository=$(ynh_read_manifest --manifest_key='upstream.code')
+    upstream_url=$(ynh_read_manifest --manifest_key='resources.sources.main.url')
+    upstream_commit=$(echo "$upstream_url" | sed -e "s|^${upstream_repository}/archive/refs/tags/\(.*\)\.tar\.gz|\1|")
+    echo "$upstream_commit"
+}
+jupyterlab_version="$(upstream_version)"
 
 #=================================================
 # EXPERIMENTAL HELPERS
