59c8415470b04daf78c1f316560a82b5056947f2
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-09-01 18:11:00 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ nodejs_version="18"
 
 jupyterlab_upstream_version() {
     upstream_repository=$(ynh_read_manifest
-    upstream_url=$(ynh_read_manifest
+    upstream_url=$(ynh_read_manifest)
     upstream_commit=$(echo "$upstream_url" | sed -e "s|^${upstream_repository}/archive/refs/tags/\(.*\)\.tar\.gz|\1|")
     echo "$upstream_commit"
 }
