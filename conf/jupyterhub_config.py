dcdbdd24091dd6c4dfd5e92eeaf16cbbc59a7075
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-12-08 16:53:33 +0100

    Update jupyterhub_config.py

--- conf/jupyterhub_config.py
+++ conf/jupyterhub_config.py
@@ -108,7 +108,7 @@ c.JupyterHub.authenticator_class = 'ldapauthenticator.LDAPAuthenticator'
 ## The public facing URL of the whole JupyterHub application.
 #
 #  This is the address on which the proxy will bind. Sets protocol, ip, base_url
-c.JupyterHub.bind_url = 'http://:__PORT____PATH_NO_SLASH__'
+c.JupyterHub.bind_url = 'http://127.0.0.1:__PORT____PATH_NO_SLASH__'
 
 ## Whether to shutdown the proxy when the Hub shuts down.
 #
